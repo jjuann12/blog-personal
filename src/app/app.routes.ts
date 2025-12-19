7a812a79fe084bcccd674f9134185c72aedc4316
juan <juan@gmail.com>
2025-12-19 13:15:42 -0300

    se crearon los componentes login y register. se personalizaron y agregaron a las rutas. esto no anda todavia con el backend ni nada.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,8 @@ import { Home } from './pages/home/home';
 import { PostList } from './pages/post-list/post-list';
 import { PostsDetail } from './pages/posts-detail/posts-detail';
 import { PostCreate } from './pages/post-create/post-create';
+import { Login } from './auth/login/login';
+import { Register } from './auth/register/register';
 
 export const routes: Routes = [
     { path: '' , component:Home},
@@ -10,4 +12,6 @@ export const routes: Routes = [
     { path: 'publicaciones' , component:PostList},
     { path: 'posts/:id' , component:PostsDetail},
     { path: 'create' , component: PostCreate},
+    { path: 'login' , component: Login},
+    { path: 'register' , component: Register},
 ];
